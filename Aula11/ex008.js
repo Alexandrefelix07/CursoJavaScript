dd7536cc281e5c7fe1b884e1c6cc83a4bc119053
Alexandre Felix <alexandrefelix07@hotmail.com>
2024-03-16 05:29:01 -0300

    condicional Script Aula 11

---
+++ Aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+
+console.log(`A velocidade do seu carro e ${vel} Km/h`);
+if (vel > 60) {
+    console.log(`Voce Ultrapasou a velocidade permitida. MULTADO!`);
+}
+console.log(`Dirija sempre usando ocinto de segurança!`); 
