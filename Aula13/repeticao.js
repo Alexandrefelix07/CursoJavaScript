192c8cb181ab30468149a1857cb8e88b47a88005
Alexandre Felix <alexandrefelix07@hotmail.com>
2024-03-17 02:24:06 -0300

    rep

---
+++ Aula13/repeticao.js
@@ -0,0 +1,11 @@
+var c = 0
+while (c < 6) {
+    console.log(`teste de respetição com while n° ${c}`);
+    c++;
+}
+var d = 0
+do {
+    console.log(`teste de respetiçãocom com do  n° ${d}`);
+    d++;
+
+} while (d < 10);
