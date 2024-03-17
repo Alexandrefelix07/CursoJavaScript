da1eb03672ea0140dac93ad1be48431d6323a116
Alexandre Felix <alexandrefelix07@hotmail.com>
2024-03-17 04:56:19 -0300

    get indexOF

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -3,3 +3,5 @@ num.push(9)
 console.log(`Nosso vetor e o: ${num}`)
 console.log(`Nosso vetor tem o tamanho de: ${num.length}`)
 console.log(`Nosso vetor em sort : ${num.sort()}`)
+let pos = num.indexOf(3);
+console.log(`Nosso vetor ta na posição : ${pos}`)
