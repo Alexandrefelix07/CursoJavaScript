e22f2b96fac5b65207630422271287e32ff3e982
Alexandre Felix <alexandrefelix07@hotmail.com>
2024-03-16 21:02:18 -0300

    fim da aula 14

--- Aula12ex/ex014/script.js
+++ Aula12ex/ex014/script.js
@@ -1,1 +1,18 @@
-alert('Ola.');
+function carregar() {
+    var msg = window.document.getElementById("msg");
+    var img = window.document.getElementById("imagem");
+    var data = new Date();
+    var hora = data.getHours();
+    msg.innerHTML = `Agora são ${hora} horas. `
+
+    if (hora >= 0 && hora < 12) {
+        img.src = '_img/dia.png'
+        document.body.style.background = '#bee6e9';
+    } else if (hora >= 12 && hora < 18) {
+        img.src = '_img/tarde.png';
+        document.body.style.background = '#fde0a4';
+    } else {
+        img.src = '_img/noite.png'
+        document.body.style.background = '#096095';
+    }
+}
