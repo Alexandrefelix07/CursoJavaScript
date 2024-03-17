6ab055423165d852fd9a14645654f9e81b3d8638
Alexandre Felix <alexandrefelix07@hotmail.com>
2024-03-17 04:57:59 -0300

    validate vetor

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -4,4 +4,8 @@ console.log(`Nosso vetor e o: ${num}`)
 console.log(`Nosso vetor tem o tamanho de: ${num.length}`)
 console.log(`Nosso vetor em sort : ${num.sort()}`)
 let pos = num.indexOf(3);
-console.log(`Nosso vetor ta na posição : ${pos}`)
+if (pos == -1) {
+    console.log(`Valor nao encontrado!`)
+} else {
+    console.log(`Nosso vetor ta na posição : ${pos}`)
+}
